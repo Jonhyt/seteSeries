34dcd18e8f50266e8b38fabc049244842df9bb46
João Pinto <Joo Pinto@JOAOPINTO-PC>
2017-05-08 11:53:52 +0100

    No output

---
+++ XML_seteSeries/XML_seteSeries/seteSeriesPars.js
@@ -0,0 +1,22 @@
+﻿var docXML;
+function inicXML(ficheiroXML) {
+    function inicXML(fichXML) {
+        //criar um objeto XMLHttpRequest
+        var xmlhttp = new XMLHttpRequest();
+        //especificar a função a ser executada quando o objeto receber cada evento "readystatechange"
+        xmlhttp.onreadystatechange = function () {
+            //a execução depende do objeto estar terminado e disponível
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                docXML = xmlhttp.responseXML;
+                inic();
+            }
+        }
+        xmlhttp.open("GET", fichXML, true);
+        xmlhttp.send();
+    }
+}
+
+//Extração de dados
+function tituloCol() {
+    return docXML.childNodes[0].getAttribute("titulo");
+}
---
+++ XML_seteSeries/XML_seteSeries/seteSeriesProcs1.js
@@ -0,0 +1,6 @@
+﻿//inicialização
+function inic() {
+    document.write(
+        tituloCol()
+    );
+}
