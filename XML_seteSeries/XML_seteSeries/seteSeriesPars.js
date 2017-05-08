230d44919e8480988e84157a7c4f6d920d297c57
João Pinto <Joo Pinto@JOAOPINTO-PC>
2017-05-08 13:00:32 +0100

    Fim da 1ª aula do trabalho

--- XML_seteSeries/XML_seteSeries/seteSeriesPars.js
+++ XML_seteSeries/XML_seteSeries/seteSeriesPars.js
@@ -18,3 +18,73 @@ function inicXML(fichXML) {
 function tituloColSeries() {
   return docXML.childNodes[0].getAttribute("titulo");
 }
+
+function numSeries() {
+    return docXML.getElementsByTagName("serie").length;
+}
+
+function tituloSerie(s) {
+    var serie = docXML.getElementsByTagName("serie")[s];
+    return serie.getAttribute("titulo");
+}
+
+function bannerSerie(s) {
+    var serie = docXML.getElementsByTagName("serie")[s];
+    return "SeteSeries/" + serie.getAttribute("banner");
+}
+
+//Dados de um canal
+function canalNomeSerie(s) {
+    var serie = docXML.getElementsByTagName("serie")[s];
+    return serie.getElementsByTagName("canalTV")[0].getAttribute("nome");
+}
+
+function canalURLSerie(s) {
+    var serie = docXML.getElementsByTagName("serie")[s];
+    return serie.getElementsByTagName("canalTV")[0].getAttribute("url");
+}
+
+function canalLogoSerie(s) {
+    var serie = docXML.getElementsByTagName("serie")[s];
+    return "SeteSeries/"+serie.getElementsByTagName("canalTV")[0].getAttribute("logo");
+}
+
+//Atributos da data de lançamento
+function diaLancSerie(s) {
+    var serie = docXML.getElementsByTagName("serie")[s];
+    return serie.getElementsByTagName("dataLanc")[0].getAttribute("dia");
+}
+
+function mesLancSerie(s) {
+    var serie = docXML.getElementsByTagName("serie")[s];
+    return serie.getElementsByTagName("dataLanc")[0].getAttribute("mes");
+}
+
+function anoLancSerie(s) {
+    var serie = docXML.getElementsByTagName("serie")[s];
+    return serie.getElementsByTagName("dataLanc")[0].getAttribute("ano");
+}
+
+//Extrai informações sobre os criadores
+function numCriaSerie(s) {
+    var serie = docXML.getElementsByTagName("serie")[s];
+    return serie.getElementsByTagName("criadores")[0].getElementsByTagName("criador").length;
+}
+
+function criaNomeSerie(s, c) {
+    var serie = docXML.getElementsByTagName("serie")[s];
+    var criador = serie.getElementsByTagName("criadores")[0].getElementsByTagName("criador")[c];
+    return criador.childNodes[0].nodeValue;
+}
+
+//Extrai as informações sobre os géneros de uma série
+function numGenSerie(s) {
+    var serie = docXML.getElementsByTagName("serie")[s];
+    return serie.getElementsByTagName("generos")[0].getElementsByTagName("genero").length;
+}
+
+function genSerie(s,g) {
+    var serie = docXML.getElementsByTagName("serie")[s];
+    var genero = serie.getElementsByTagName("generos")[0].getElementsByTagName("genero")[g];
+    return genero.childNodes[0].nodeValue;
+}
--- XML_seteSeries/XML_seteSeries/seteSeriesProcs1.js
+++ XML_seteSeries/XML_seteSeries/seteSeriesProcs1.js
@@ -1,7 +1,28 @@
 ﻿//seteSeriesProcs1.js *********************************************************
 //inicialização
 function inic() {
-  document.write(
-    tituloColSeries()
-  );
+    listTest1();
+}
+
+//TESTE: listagem de banner, logo do canal, data de lançamento e criadores para cada serie
+function listTest1() {
+    var i, nS = numSeries();
+    document.write('<h1>'+tituloColSeries()+'</h1>')
+    for (i = 0; i < nS; i++) {
+        document.write(
+            "<hr/>" +'<h2>' + tituloSerie(i) + '</h2><img src="' + bannerSerie(i) + '" height="80"/><img src="' + canalLogoSerie(i) + '" height="80"/><br/><b>Data de lançamento: </b>'+
+            diaLancSerie(i) + '/' +mesLancSerie(i) + '/' + anoLancSerie(i)+'</br>'+'<b>Criado por: </b>'
+        );
+        for (var j = 0; j < numCriaSerie(i) ; j++) {
+            document.write(criaNomeSerie(i, j));
+            if (j == 0 && numCriaSerie(i) > 1)
+                document.write(", ");
+        }
+        document.write('<br/><b>Género: </b>');
+        for (var j = 0; j < numGenSerie(i) ; j++) {
+            document.write(genSerie(i, j));
+            if (j != numGenSerie(i)-1 && numGenSerie(i) > 1)
+                document.write(", ");
+        }
+    }
 }
